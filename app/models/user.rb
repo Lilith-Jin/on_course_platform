03b3d1157c734e5a613ca6027d1f62e75f4ec1bb
Lilith <li6253462@gmail.com>
2022-02-19 19:59:03 +0800

    add user default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  rolify
+  has_and_belongs_to_many :roles, :join_table => :users_roles
+  after_create :assign_defeat_role
+
+  def assign_defeat_role
+    self.add_role(:admin) if self.roles.blank?
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
